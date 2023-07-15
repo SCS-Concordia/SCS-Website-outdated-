a7782774ba00049a84616c6f09e7156190b43e3a
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2023-07-15 19:38:19 -0400

    fixed the navbar

--- templates/navbar.js
+++ templates/navbar.js
@@ -10,17 +10,21 @@ const navbar = `
         </a>
         <div id="site-links">
             <div class="site-link"><a href="${pathAdjust2}team.html">Team</a></div>
+            <div>|</div>
             <div class="site-link"><a href="${pathAdjust2}sponsors.html">Sponsors</a></div>
+            <div>|</div>
             <div class="site-link"><a href="${pathAdjust2}events.html">Events</a></div>
+            <div>|</div>
             <div class="site-link"><a href="${pathAdjust2}competitions.html">Competitions</a></div>
-            <div><a id="contact-link" href="">Contact</a></div>
-
+            <div>|</div>
+            <div><a class="site-link" href="">Contact</a></div>
+            <div>|</div>
             <a id="instagram-link" href="https://www.instagram.com/scs.concordia/" target="_blank">
-            <img id="instagram" src=${pathAdjust1}assets/some_icons/instagram.png>
+            <img id="instagram" src=${pathAdjust1}assets/instagram.svg>
             </a>
-
+            <div>|</div>
             <a id="discord-link" href="https://discord.gg/nuv4UC6n" target="_blank">
-            <img id="discord" src=${pathAdjust1}assets/some_icons/discord.png>
+            <img id="discord" src=${pathAdjust1}assets/discord-icon.svg>
             </a>
             
             <img id="hamburger" src="${pathAdjust1}assets/hamburger.png" />
